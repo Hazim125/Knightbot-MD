909bd1dadd2286f89dd6346bb64f80f48cfd6ced
Hazim125 <aalmhfzt@gmail.com>
2025-04-26 14:08:01 +0200

    Update settings.js

--- settings.js
+++ settings.js
@@ -3,7 +3,7 @@ const settings = {
   author: 'Bot',
   botName: "Knight Bot",
   botOwner: 'Darkzenin', // Your name
-  ownerNumber: '7 926 739-42-64', //Your number
+  ownerNumber: '249966162613', //Your number
   giphyApiKey: 'qnl7ssQChTdPjsKta2Ax2LMaGXz303tq',
   commandMode: "public",
   description: "This is a bot for managing group commands and automating tasks.",
